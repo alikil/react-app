fe49209f75fea001934a816c10e2ab7f209d7ecd
alikil <Serhii_Kalachnikov@outlook.com>
2019-04-25 12:16:47 +0300

    router main redirect

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Index from "./index/index.js";
 import Sidebar from "./index/sidebar";
@@ -21,7 +21,7 @@ export default function AppRouterWay() {
     {
       path: "/",
       exact: true,
-      main: () => <h2><Index /></h2>
+      main: () => <h2><Redirect to="/index"/></h2>
     },
     {
       path: "/index",
